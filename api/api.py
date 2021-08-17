6f0e1f125983d6abaf69277b3516417da5d2b6e3
nnwwbb <49573256+nnwwbb@users.noreply.github.com>
2021-08-17 02:00:32 +0200

    only root path is real

--- api/api.py
+++ api/api.py
@@ -57,9 +57,9 @@ def main():
             description='Something something moon',
             version='0.1',
             root_path=cfg['nginx-settings']['root_path'],
-            docs_url=cfg['nginx-settings']['docs_url'],
-            redoc_url=cfg['nginx-settings']['redoc_url'],
-            openapi_url=cfg['nginx-settings']['openapi_url']
+            # docs_url=cfg['nginx-settings']['docs_url'],
+            # redoc_url=cfg['nginx-settings']['redoc_url'],
+            # openapi_url=cfg['nginx-settings']['openapi_url']
         )
     else:
         app = FastAPI(
